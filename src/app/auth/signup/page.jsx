d71cc43f0ee7df2337c3b89d38160154a3ad31f3
MD. Sabbir Hossain <sabbirhossain8721@gmail.com>
2026-07-27 16:13:06 +0600

    5 Implement sign up form and sign up user

--- src/app/auth/signup/page.jsx
+++ src/app/auth/signup/page.jsx
@@ -1,9 +1,101 @@
-
+"use client";
+import { authClient } from "@/lib/auth-client";
+import { Check } from "@gravity-ui/icons";
+import { Button, Description, FieldError, Form, Input, Label, TextField } from "@heroui/react";
 
 const SignUpPage = () => {
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+       const userData = Object.fromEntries(formData.entries());
+       console.log("Form submitted with:", userData);
+
+        const { data, error } = await authClient.signUp.email({
+            name: userData.name,
+            email: userData.email,
+            password: userData.password,
+        });
+
+        console.log("data:", data);
+        console.log("error:", error);
+
+        if (error) {
+            console.error(JSON.stringify(error, null, 2));
+        }
+
+       console.log("Sign up response:", {data, error});
+    };
+
     return (
         <div>
             <h2>Please sign up</h2>
+            <Form className="flex w-96 flex-col gap-4" onSubmit={onSubmit}>
+
+                <TextField isRequired
+                name="name"
+                validate={(value) => {
+                    if(value.length < 3) {
+                        return "Name must be at least 3 characters";
+                    }
+                    return null;
+                }}
+                >
+                    <Label>Name</Label>
+                    <Input name="name" placeholder="Enter your name" />
+                    <FieldError></FieldError>
+                </TextField>
+
+                <TextField
+                    isRequired
+                    name="email"
+                    type="email"
+                    validate={(value) => {
+                        if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+                            return "Please enter a valid email address";
+                        }
+                        return null;
+                    }}
+                >
+                    <Label>Email</Label>
+                    <Input name="email" placeholder="Enter your email address" />
+                    <FieldError />
+                </TextField>
+
+                <TextField
+                    isRequired
+                    minLength={8}
+                    name="password"
+                    type="password"
+                    validate={(value) => {
+                        if (value.length < 8) {
+                            return "Password must be at least 8 characters";
+                        }
+                        if (!/[A-Z]/.test(value)) {
+                            return "Password must contain at least one uppercase letter";
+                        }
+                        if (!/[0-9]/.test(value)) {
+                            return "Password must contain at least one number";
+                        }
+                        return null;
+                    }}
+                >
+                    <Label>Password</Label>
+                    <Input name="password" placeholder="Enter your password" />
+                    <Description>Must be at least 8 characters with 1 uppercase and 1 number</Description>
+                    <FieldError />
+                </TextField>
+
+                <div className="flex gap-2">
+                    <Button type="submit">
+                        <Check />
+                        Submit
+                    </Button>
+                    <Button type="reset" variant="secondary">
+                        Reset
+                    </Button>
+                </div>
+            </Form>
         </div>
     );
 };
